548e49ca749a3cc0520ed3554e82f1435d10aef7
Blockhore <blockhore.team@gmail.com>
2025-07-19 12:21:17 +0700

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { Buffer } from 'buffer';
+import * as buffer from 'buffer';  // <== ini diubah
 import App from './App.tsx';
 import './index.css';
 
 // Make Buffer available globally for browser compatibility
-(window as any).Buffer = Buffer;
+(window as any).Buffer = buffer.Buffer;
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
