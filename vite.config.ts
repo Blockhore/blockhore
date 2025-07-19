c1ffade1bc40194fb2bc4bfb19e80f9c25b9e1e0
Blockhore <blockhore.team@gmail.com>
2025-07-19 12:11:49 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,38 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
+import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      buffer: 'buffer/',  // perhatikan slash '/' di akhir supaya resolve ke package dengan benar
+      process: 'process/browser',
+    },
+  },
   optimizeDeps: {
     esbuildOptions: {
-      // Enable esbuild polyfill plugins
       define: {
-        global: 'globalThis'
+        global: 'globalThis',
       },
       plugins: [
         NodeGlobalsPolyfillPlugin({
           buffer: true,
         }),
+        NodeModulesPolyfillPlugin(),
+      ],
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [
+        rollupNodePolyFill()
       ],
     },
   },
   define: {
     'process.env': {},
-  },
-  resolve: {
-    alias: {
-      buffer: 'buffer',
-    },
   },
 });
